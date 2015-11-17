121f5b1b0990f3d9ce828733a4c340bf88996bf2
NoraAl <nemseo@gmail.com>
2015-11-17 16:20:38 -0500

    feat: image picker is done
    - an image can be choosen from library or camera if available,
    - the image is presented in UIImageView
    - delegate for UIImagePickerController object is the same class

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        shareButton.enabled = false
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func libraryButton(sender: UIBarButtonItem) {
+        pickAnImage(fromLibrary: true)
+    }
+    
+    @IBAction func cameraButton(sender: UIBarButtonItem) {
+        pickAnImage(fromLibrary: false)
     }
 
+    @IBAction func shareButton(sender: UIBarButtonItem) {
+    }
+    
+    func pickAnImage(fromLibrary fromLibrary: Bool) {
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        
+        if(fromLibrary){
+            imagePickerViewController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        else {
+            imagePickerViewController.sourceType = UIImagePickerControllerSourceType.Camera
+        }
+        presentViewController(imagePickerViewController, animated: true, completion: nil)
+    }
+    
+    //imagePickerViewController delegate mehtods
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            shareButton.enabled = true
+        }
+        else {
+            
+            print("image cannot be selected properly")
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
-
